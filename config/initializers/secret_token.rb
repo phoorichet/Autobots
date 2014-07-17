3117e3aa6543ccb61fa214089184d5b616ab09ec
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2014-07-17 14:28:35 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autobots::Application.config.secret_key_base = 'c9d7a535fba7e22261d5880aee60e847c7a34bf53deebd528319dd2ac3ddcaab06e2a484c72ff61f906e1ed8d961fe4f233c79ea9b7c4d500a265e77b947fb16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Autobots::Application.config.session_store :cookie_store, key: '_Autobots_session'
